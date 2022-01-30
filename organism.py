4104cfad6985091029b18bf5414c613165152c93
wesleydashner <wdashner11@gmail.com>
2022-01-29 16:31:45 -0800

    refactored and made it so organisms cannot be in the same position

--- organism.py
+++ organism.py
@@ -1,23 +1,14 @@
 from typing import Tuple
-from direction import Direction
-from random import choice, randint
+from random import choice
 
 
 class Organism:
-    def __init__(self, x: int, y: int, habitat_width: int, color: Tuple[int, int, int] = (255, 255, 255)):
+    def __init__(self, x: int, y: int, color: Tuple[int, int, int] = (255, 255, 255)):
         self.x = x
         self.y = y
         self.color = color
-        self.habitat_width = habitat_width
 
-    def move(self, direction: Direction):
-        delta_x = direction.value[0]
-        delta_y = direction.value[1]
-        new_x = self.x + delta_x
-        new_y = self.y + delta_y
-        if 0 < new_x < self.habitat_width and 0 < new_y < self.habitat_width:
-            self.x = new_x
-            self.y = new_y
-
-    def random_move(self):
-        self.move(choice(list(Direction)))
+    def get_random_move(self, possible_directions):
+        if possible_directions == []:
+            return None
+        return choice(possible_directions)
--- simulation.py
+++ simulation.py
@@ -1,30 +1,69 @@
+from random import random, shuffle
+from typing import List, Tuple, Optional
+
 from renderer import Renderer
 from organism import Organism
-from random import randint
+from direction import Direction
 
 
 class Simulation:
     def __init__(self):
-        self.habitat_width = 200
-        self.organisms = []
-        self.board = self.get_board()
+        self.habitat_width: int = 200
+        self.organisms: List[Organism] = []
+        self.habitat: List[List[Optional[Organism]]] = self.get_empty_habitat()
+        self.renderer: Renderer = Renderer(5, self.habitat_width)
+        self.spawn_organisms()
 
     def run(self):
-        block_width = 5
-        population_size = 1000
-        renderer = Renderer(block_width, self.habitat_width)
-        self.organisms = [
-            Organism(randint(0, self.habitat_width - 1), randint(0, self.habitat_width - 1), self.habitat_width,
-                     (randint(0, 200), randint(0, 200), randint(0, 200)))
-            for _ in range(population_size)]
         while True:
-            for organism in self.organisms:
-                organism.random_move()
-            board = self.get_board()
-            renderer.render(board)
+            self.renderer.render(self.get_board_for_renderer())
+            self.move_organisms()
+            print(len(self.organisms))
 
-    def get_board(self, default_color=(0, 0, 0)):
-        board = [[default_color for _ in range(self.habitat_width)] for _ in range(self.habitat_width)]
+    def move_organism(self, organism: Organism, direction: Direction):
+        if direction is not None:
+            self.habitat[organism.y][organism.x] = None
+            delta_x, delta_y = direction.value
+            organism.x += delta_x
+            organism.y += delta_y
+            self.habitat[organism.y][organism.x] = organism
+
+    def move_organisms(self):
+        # shuffle organisms, so first organisms don't always have an advantage of getting a desirable spot
+        shuffle(self.organisms)
         for organism in self.organisms:
-            board[organism.y][organism.x] = organism.color
+            move = organism.get_random_move(self.get_possible_directions(organism))
+            self.move_organism(organism, move)
+
+    def get_possible_directions(self, organism: Organism):
+        possible_directions = []
+        for direction in Direction:
+            delta_x, delta_y = direction.value
+            if self.is_empty(organism.x + delta_x, organism.y + delta_y):
+                possible_directions.append(direction)
+        return possible_directions
+
+    def is_empty(self, x, y):
+        return 0 <= x < self.habitat_width and 0 <= y < self.habitat_width and self.habitat[y][x] is None
+
+    def get_empty_habitat(self):
+        return [[None for _ in range(self.habitat_width)] for _ in range(self.habitat_width)]
+
+    def get_organism(self, x: int, y: int):
+        return Organism(x, y)
+
+    def spawn_organisms(self, spawn_probability: float = 0.01):
+        for y in range(self.habitat_width):
+            for x in range(self.habitat_width):
+                if random() < spawn_probability:
+                    new_organism = self.get_organism(x, y)
+                    self.habitat[y][x] = new_organism
+                    self.organisms.append(new_organism)
+
+    def get_board_for_renderer(self, default_color: Tuple[int, int, int] = (0, 0, 0)):
+        board = [[default_color for _ in range(self.habitat_width)] for _ in range(self.habitat_width)]
+        for y in range(self.habitat_width):
+            for x in range(self.habitat_width):
+                if self.habitat[y][x] is not None:
+                    board[y][x] = self.habitat[y][x].color
         return board
